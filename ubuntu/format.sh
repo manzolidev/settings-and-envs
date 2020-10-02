24273024fb09aef2e0a89ef3f7a1958b2dd65502
Richard Manzoli <manzolirch@gmail.com>
2020-10-02 12:48:20 -0300

    fix

--- ubuntu/format.sh
+++ ubuntu/format.sh
@@ -59,3 +59,22 @@ sudo snap install --classic figma-linux
 
 echo 'Installing Redis'
 sudo apt-get install redis-server
+
+echo 'Installing VSCode Extensions'
+code --install-extension adpyke.codesnap &&
+code --install-extension christian-kohler.path-intellisense &&
+code --install-extension CoenraadS.bracket-pair-colorizer &&
+code --install-extension cssho.vscode-svgviewer &&
+code --install-extension dbaeumer.vscode-eslint &&
+code --install-extension eamodio.gitlens &&
+code --install-extension EditorConfig.EditorConfig &&
+code --install-extension Equinusocio.vsc-community-material-theme &&
+code --install-extension Equinusocio.vsc-material-theme &&
+code --install-extension equinusocio.vsc-material-theme-icons &&
+code --install-extension esbenp.prettier-vscode &&
+code --install-extension kamikillerto.vscode-colorize &&
+code --install-extension mikestead.dotenv &&
+code --install-extension octref.vetur &&
+code --install-extension pcanella.marko &&
+code --install-extension PKief.material-icon-theme &&
+code --install-extension yzhang.markdown-all-in-one
