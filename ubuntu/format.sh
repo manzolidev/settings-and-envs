dbefb9b55ff6ac8853591027467240a51b8f6eec
Richard Manzoli <manzolirch@gmail.com>
2020-10-02 11:32:37 -0300

    Update format.sh

--- ubuntu/format.sh
+++ ubuntu/format.sh
@@ -2,61 +2,31 @@
 
 clear
 
-echo '----------------------------------------------------------------------------------------'
-echo 'UBUNTU DEVELOPMENT SETUP - INSTALATION BASH \n\n'
-echo '----------------------------------------------------------------------------------------'
+echo 'Ubunto dev setup'
 
-echo '----------------------------------------------------------------------------------------'
-echo 'UPDATE UBUNTU'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Update ubuntu'
 sudo apt update 
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING VSCODE'
-echo '----------------------------------------------------------------------------------------'
+echo 'Installing VSCode'
+sudo snap install --classic code
 
-sudo apt update
-sudo sudo apt install software-properties-common apt-transport-https wget
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O| sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code
-
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING VIM'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Installing VIM'
 sudo apt install vim
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING GIT'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Installing Git'
 sudo apt install git
 git config --global user.name "Richard Manzoli"
 git config --global user.email "manzolirch@gmail.com"
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING CHROME'
-echo '----------------------------------------------------------------------------------------'
+echo 'Installing Brave'
+sudo snap install brave
 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-- wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install google-chrome-stable
-
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING ASDF'
-echo '----------------------------------------------------------------------------------------'
-
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+echo 'Installing Asdf'
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING NODEJS'
-echo '----------------------------------------------------------------------------------------'
+echo 'Installing NodeJS'
 
 sudo apt-get install dirmngr
 sudo apt-get install gpg
@@ -64,43 +34,20 @@ sudo apt-get install curl
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf plugin add nodejs
-asdf install nodejs 14.1.0
+asdf install nodejs 14.13.0
 asdf install nodejs 12.16.3
 asdf local nodejs 12.16.3
 asdf global nodejs 12.16.3
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING YARN'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Installing Yarn'
 npm i -g yarn
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING MATERIAL THEME IN TERMINAL'
-echo '----------------------------------------------------------------------------------------'
-
-wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
-90
-
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING ZHS AND OH MY ZHS'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Installing ZSH'
 sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo apt-get install dconf-cli
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-plugins=(zsh-autosuggestions) (inside ~/.zshrc)
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING TWEAK TOOL'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Installing Tweak tool'
 sudo add-apt-repository universe
 sudo apt install gnome-tweak-tool
 
-echo '----------------------------------------------------------------------------------------'
-echo 'INSTALLING POSTMAN'
-echo '----------------------------------------------------------------------------------------'
-
+echo 'Installing Postman'
 sudo snap install postman
